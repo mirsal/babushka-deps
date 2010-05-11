c86eedeedc85d35fb3c0cc8f95ab7c5889f8354b
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-05-11 22:49:58 +0200

    Remove copy / paste error

--- base-system.rb
+++ base-system.rb
@@ -36,22 +36,3 @@ dep 'configured fstab', :for => :linux do
     	render_erb 'base-system/fstab.erb', :to => target, :sudo => !File.writable?(File.dirname(target))
     }
 end
-
-pkg 'mount' do
-  cfg '/etc/fstab'
-  setup {
-    define_var :dhcp_network,
-      :type => :ip_range,
-      :default => '10.0.1.x',
-      :message => "What network range would you like to serve DHCP on?"
-
-    set :dhcp_subnet, L{ Babushka::IPRange.new(var(:dhcp_network)).subnet }
-    set :dhcp_broadcast_ip, L{ Babushka::IPRange.new(var(:dhcp_network)).broadcast }
-
-    define_var :dns_domain, :message => "The network's domain", :default => 'example.org'
-    define_var :dns_server_ip, :message => "The DNS server itself", :default => L{ Babushka::IPRange.new(var(:dhcp_network)).first }
-    define_var :dhcp_router_ip, :message => "Default gateway", :default => L{ var :dns_server_ip }
-    define_var :dhcp_start_address, :message => "DHCP starting address", :default => L{ Babushka::IP.new(var(:dns_server_ip)).next }
-    define_var :dhcp_end_address, :message => "DHCP ending address", :default => L{ Babushka::IPRange.new(var(:dhcp_network)).last.prev }
-  }
-end
