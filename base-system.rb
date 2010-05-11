2f69eb9172c2e93050c531bfd88d8a236d4df479
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-05-11 21:34:52 +0200

    More correct fstab dep code

--- base-system.rb
+++ base-system.rb
@@ -27,12 +27,12 @@ dep 'configured fstab', :for => :linux do
     }
     meet {
 	define_var :rootfs, :message => "/ (root) file system device node ?", :default => '/dev/sda1'
-	define_var :bootfs, :message => "/boot file system device node ? (type none for no separate partition)", :default => 'none'
-	define_var :swapfs, :message => "swap  file system device node ? (type none for no separate partition)", :default => 'none'
-	define_var :tmpfs,  :message => "/tmp  file system device node ? (type none for no separate partition)", :default => 'none'
-	define_var :varfs,  :message => "/var  file system device node ? (type none for no separate partition)", :default => 'none'
-	define_var :usrfs,  :message => "/usr  file system device node ? (type none for no separate partition)", :default => 'none'
-	define_var :homefs, :message => "/home file system device node ? (type none for no separate partition)", :default => 'none'
+	define_var :swapfs, :message => "swap  file system device node ?",    :default => '/dev/sda2'
+	define_var :bootfs, :message => "/boot file system device node ? (type \"#unused\" for no separate partition)", :default => '#unused'
+	define_var :tmpfs,  :message => "/tmp  file system device node ? (type \"#unused\" for no separate partition)", :default => '#unused'
+	define_var :varfs,  :message => "/var  file system device node ? (type \"#unused\" for no separate partition)", :default => '#unused'
+	define_var :usrfs,  :message => "/usr  file system device node ? (type \"#unused\" for no separate partition)", :default => '#unused'
+	define_var :homefs, :message => "/home file system device node ? (type \"#unused\" for no separate partition)", :default => '#unused'
     	render_erb 'base-system/fstab.erb', :to => target, :sudo => !File.writable?(File.dirname(target))
     }
 end
