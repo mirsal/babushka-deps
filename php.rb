7cd9b36674839392319eb5b0aea38a2eeea37dc5
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-05-13 22:20:53 +0200

    Install the php command line interpreter

--- php.rb
+++ php.rb
@@ -3,12 +3,17 @@ pkg 'php cgi', :for => :ubuntu  do
   provides []
 end
 
+pkg 'php cli', :for => :ubuntu do
+  installs { via :apt, 'php5-cli' }
+  provides []
+end
+
 dep 'php for lighttpd', :for => :linux do
   requires 'lighttpd webserver', 'fastcgi', 'php cgi'
 end
 
 dep 'php' do
   setup {
-    requires "php for #{var(:webserver, :default => 'lighttpd', :message => 'Setup php for ')}" 
+    requires 'php cli', "php for #{var(:webserver, :default => 'lighttpd', :message => 'Setup php for ')}" 
   }
 end
