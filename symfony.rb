cfd87b8997247f3e70bb436778b9d4032b3915da
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-05-13 21:57:17 +0200

    Symfony project permissions dep stub

--- symfony.rb
+++ symfony.rb
@@ -4,11 +4,7 @@ dep 'symfony vhost' do
   }
 end
 
-dep 'symfony app' do
-  setup {
-    requires 'php', 'symfony vhost' 
-  }
-
+dep 'cloned symfony project repo' do
   met? {
     (var(:document_root) / 'symfony').exist?
   }
@@ -17,5 +13,26 @@ dep 'symfony app' do
     in_dir var(:document_root) do
       sudo "git clone #{var(:project_git_repository)} ."
     end
+  }
+end
+
+def symfony_task task, args
+  in_dir var(:document_root) do
+    sudo "php symfony #{task} #{args}"
+  end
+end
+
+dep 'permissions set' do
+  requires 'cloned symfony project repo'
+  met? {
+    in_dir var(:document_root) do
+      File.stat('cache').mode['777'].nil? && log 'cache perms ok'
+    end
+  }
+end
+
+dep 'symfony app' do
+  setup {
+    requires 'php', 'symfony vhost', 'cloned symfony project repo', 'permissions set' 
   }
 end
